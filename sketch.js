bb87d346b5e39115ecf1136bdbf2da83086b2cef
Eliot Cholerton-Hill <eliot.cholerton-hill@localhost.localdomain>
2019-07-24 23:31:17 +0100

    Beautified page

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 var bubble = function (b) {
 
   b.setup = function () {
-    b.createCanvas(640, 480);
+    b.createCanvas(320, 240);
     b.frameRate(10);
     b.fill(102);
-    b.c = new Collection(b, b.width, b.height, 10);
+    b.c = new Collection(b, b.width, b.height, 5);
     b.c.generateBars();
     b.c.shuffleBars();
     b.bSort = new BubbleSort(b.c);
@@ -23,10 +23,10 @@ var bubble = function (b) {
 var selection = function (b) {
 
   b.setup = function () {
-    b.createCanvas(640, 480);
+    b.createCanvas(320, 240);
     b.frameRate(10);
     b.fill(102);
-    b.c = new Collection(b, b.width, b.height, 10);
+    b.c = new Collection(b, b.width, b.height, 5);
     b.c.generateBars();
     b.c.shuffleBars();
     b.sSort = new SelectionSort(b.c);
@@ -42,6 +42,6 @@ var selection = function (b) {
   }
 }
 
-var myp5_1 = new p5(bubble);
-var myp5_2 = new p5(selection);
+var container1 = new p5(bubble, "bubble");
+var container2 = new p5(selection, "selection");
 //let myp5 = new p5(bubble);
